a62b8bce44e1de3515b54780526694ff7d3acc9b
yahyojondev <nabijonovdeveloper@gmail.com>
2024-01-12 10:10:43 +0500

    simple

---
+++ main.js
@@ -0,0 +1,77 @@
+// let arr = new Array[10,20,30];
+
+// Array.prototype.sum = function (){
+//   let sum = 0;
+//   for(let el of this){
+//     sum += el;
+//   }
+//   return sum;  
+// }
+
+// let res = arr.sum();
+// console.log(arr);
+
+// console.log(res);
+
+// console.log([10,20,80].sum());
+
+
+
+
+// Array.prototype.fltr = function(callback){
+//   let res =[];
+//   for(let i =0; i < this.length; i++){
+//     let check = callback(this[i],i,this);
+//     if(check){
+//       res.push(this[i])
+//     }
+//   }
+//   return res;
+// }
+
+// let arr = [5,4,6,8,7];
+
+// let even = arr.filter((el) => el % 2 ===0);
+// console.log(even);
+
+
+// js-13.1
+
+// Array.prototype.trm = function(string, value){
+//     this.value= value
+//     string.trim(){
+//       if(value.length() <= 2){
+//         return value
+//       }
+//     }
+// }
+// let arr = " Hello world ";
+
+// let even = arr.trim();
+
+// console.log(even);
+
+// js-13-2
+
+// Array.prototype.upperCase = function(arr){
+//   let res = [];
+//   for(let i =0; i< this.length; i++){
+//     if(this[i] == this[i].toLowerCase){
+//           res += this[i].toUpperCase;
+//     }
+//   }
+//   return res;
+// }
+
+// let arr = "yahyojon";
+
+// let even = arr.toUpperCase
+// console.log(even);
+
+// js-13-3
+
+
+
+
+
+
