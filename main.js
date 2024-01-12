2fd8f2c098c5a72145b08b4b4cf8d30bb81bc0bb
yahyojondev <nabijonovdeveloper@gmail.com>
2024-01-12 10:49:56 +0500

    simple

--- main.js
+++ main.js
@@ -70,6 +70,122 @@
 
 // js-13-3
 
+// Array.prototype.has = functon(arr){
+//   let res = [];
+//   if(arr.includes(arr.el)){
+//     true
+//   }
+//  return res;
+// }
+
+// let arr = ["banana","orange","ptatoes","apple"];
+
+// let even = arr.includes("orange");
+// console.log(even)
+
+// js-13-4
+
+
+//  Array.prototype.has = functon(arr){
+//   let res = [];
+// for(let el of arr){
+
+  //   if(arr.slice(2,3)){
+  //    res += el
+  //   }
+// }
+//  return res;
+// }
+// let arr = 'yahyojon';
+
+// let even = arr.slice(2,3);
+// console.log(even)
+
+// js-13-5
+
+
+//  Array.prototype.has = functon(arr){
+//   let res = [];
+// for(let el of arr){
+
+  //   if(arr.slice(2,3)){
+  //    res += el
+  //   }
+// }
+//  return res;
+// }
+// let arr = 'yahyojon';
+
+// let even = arr.repeat(2)
+// console.log(even)
+
+// js-13-6
+
+// Array.prototype.fixed = function(number){
+//   let res = [];
+//   for(let el of number){
+//     res += el.tofixed()
+//   }
+//   return res ;
+// }
+
+// let number = 222.5;
+// let even = tofixed(number);
+// console.log(even);
+
+// js-13-7
+
+//  Array.prototype.fixed = function(number){
+//   let res = [];
+//   for(let el of number){
+//     res += el.round(2)
+//   }
+//   return res ;
+// }
+
+// let number = "5.10.102";
+// let even = round(number);
+// console.log(even);
+
+// Js-13-8
+
+// Array.prototype.issquare = function(){
+//   if (Number.isInteger(Math.sqrt(n))) {
+//     return true;
+// } else {
+//     return false;
+// }
+// }
+
+// js-13-9
+// Array.prototype.count = function(arr){
+//   let sum = 0;
+//   for(let i=0; i< arr.length; i++){
+//     sum += arr[i]
+//   }
+//   return sum;
+// }
+
+// js-13-10
+
+// Array.prototype.sum = function(arr){
+//   let sum = 0;
+//   for(let el of arr){
+//     if(el >0){
+//       sum += el;
+//     }
+//   }
+//   return sum;
+// }
+
+// let arr = (1,2,3,45)
+// for(let i= 0; i< arr.length; i++){
+//   sum += el>0;
+//   return sum;
+// }
+// console.log(sum);
+
+
 
 
 
